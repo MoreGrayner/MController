0a077c4e8a16ce6f321cf6866fa18b6aa103e09c
MoreGrayner <peargrayinyt@gmail.com>
2025-02-07 20:48:32 +0900

    e

---
+++ src/main/java/io/github/moregrayner/plugins/controller.java
@@ -0,0 +1,17 @@
+package io.github.moregrayner.plugins;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class controller extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
